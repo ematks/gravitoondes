49dd1c58af34c03c91d8670f4655606eaaf062a8
Florent Robinet <robinet@lal.in2p3.fr>
2019-03-18 08:48:40 +0100

    add containers to tiling

--- src/tiler.cc
+++ src/tiler.cc
@@ -6,7 +6,13 @@ using namespace std;
 int main (int argc, char* argv[]){
 
   // tiling structure
-  Tiling *T = new Tiling(10, 16, 500, 1236580000, 1236580010);
+  Tiling *T = new Tiling(5, 16, 500, 1236580000, 1236580010);
+
+  for(int f=0; f<T->GetNBands(); f++){
+    for(int t=0; t<T->GetNtiles(f); t++){
+      T->SetTileContent(f,t, t%2);
+    }
+  }
 
   // draw tiling
   TCanvas *mycanvas = new TCanvas("MyCanvas", "MyCanvas", 0, 0, 800, 600);
@@ -14,6 +20,7 @@ int main (int argc, char* argv[]){
   mycanvas->SetGridx();
   mycanvas->SetGridy();
   T->Draw();
+  mycanvas->SetLogy();
   mycanvas->Print("./my_spectrogram.png");
 
   delete mycanvas;
--- src/tiling.h
+++ src/tiling.h
@@ -1,8 +1,11 @@
 #ifndef __tiling__
 #define __tiling__
 
+#include "TMath.h"
 #include "TCanvas.h"
 #include "TH2D.h"
+#include "fftw3.h"
+#include <iostream>
 
 using namespace std;
 
@@ -53,6 +56,11 @@ class Tiling {
     return tile_map->GetYaxis()->GetBinLowEdge(aBandIndex+1);
   };
 
+  // returns the frequency of a band
+  inline double GetBandFrequency(const int aBandIndex){
+    return tile_map->GetYaxis()->GetBinCenter(aBandIndex+1);
+  };
+
   // returns the frequency end of a band
   inline double GetBandEnd(const int aBandIndex){ 
     return tile_map->GetYaxis()->GetBinUpEdge(aBandIndex+1);
@@ -68,10 +76,38 @@ class Tiling {
     return tile_map->Draw("COLZ");
   }
 
+  void SetTileContent(const int aFrequencyIndex,
+		      const int aTimeIndex,
+		      const double aContent);
+
+  inline int GetNtiles(const int aFrequencyIndex){
+    return Nt[aFrequencyIndex];
+  }
+  
+  inline int GetFrequencyIndex(const double aFrequency){
+    return tile_map->GetYaxis()->FindBin(aFrequency)-1;
+  };
+
+  inline int GetTimeIndex(const int aFrequencyIndex, const double aTime){
+    return (tile_map->GetXaxis()->FindBin(aTime)-1)/bandMultiple[aFrequencyIndex];
+  };
+
+  void ProjectData(fftw_complex *aDataFft);
+
+
  private:
 
+  double QPrime;
   TH2 *tile_map; // tiling histogram
-  int Nt;        // number of time bins
+  int *Nt;       // number of time bins
+
+  int *bandMultiple;      ///< number of bins in one tile
+  int *bandWindowSize;    ///< band bisquare window size
+  double **bandWindow_r;  ///< band bisquare windows (real)
+  double **bandWindow_i;  ///< band bisquare windows (imaginary)
+  fftw_complex **bandin;
+  fftw_complex **bandout;
+  fftw_plan *bandFFT;          ///< band ffts
 
 };
 
